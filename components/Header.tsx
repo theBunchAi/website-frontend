50c27d81703ea0f8431168d722e6d95a6fcfcb40
Sai Rohit <sairohit11.s@gmail.com>
2023-07-26 20:33:25 +0530

    img upscale

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,3 @@
-// import { useEffect } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import useHeaderRef from "@/hooks/use-header-ref";
 
@@ -7,28 +5,6 @@ export default function Header() {
   const headerRef = useHeaderRef();
   return (
     <header className={`header`} ref={headerRef}>
-      <Link href="/" className="logo-container">
-        <div className="img-container logo-shapes">
-          <Image
-            src="/assets/images/logo.png"
-            alt="Logo"
-            fill
-            priority
-            sizes="100%"
-            className="logo-img"
-          />
-        </div>
-        <div className="img-container logo-word">
-          <Image
-            src="/assets/images/logo-word.png"
-            alt="Logo"
-            fill
-            priority
-            sizes="100%"
-            className="logo-word-img"
-          />
-        </div>
-      </Link>
       <nav className="nav">
         <Link href="/" className="nav-links">
           Home
