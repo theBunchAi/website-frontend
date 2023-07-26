ea7e11218614c747cf9e0d2d377cb0b0804aa14d
Sai Rohit <sairohit11.s@gmail.com>
2023-07-26 20:23:32 +0530

    upscale

--- components/page-heading.tsx
+++ components/page-heading.tsx
@@ -64,7 +64,7 @@ export default function PageHeading({
     <section className="page-heading-sec" ref={sectionRef}>
       <div className="img-container bg-img-container">
         <Image
-          src="/assets/images/dt.png"
+          src="/assets/images/dt-transformed.png"
           alt="3D Swirl"
           fill
           priority
