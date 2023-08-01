07c7a47d7f12ec73918c8646b90a8bbdc4b9ff74
Gobind Singh <android.gobind@gmail.com>
2023-08-01 20:36:12 +0530

    home page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-const path = require("path");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
   },
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 import Footer from "@/components/footer";
-import "@/styles/styles.scss";
+import "@/styles/styles.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
