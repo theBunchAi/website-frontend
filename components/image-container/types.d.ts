aeeaa0325abadb45e079b086bc6de4ce861109ca
Gobind Singh <android.gobind@gmail.com>
2023-08-02 22:42:56 +0530

    try2

--- components/image-container/types.d.ts
+++ components/image-container/types.d.ts
@@ -2,4 +2,5 @@ import type { ImgHTMLAttributes } from "react";
 export interface ImageContainerProps
   extends ImgHTMLAttributes<HTMLImageElement> {
   containerClassName?: string;
+  priority?: boolean;
 }
