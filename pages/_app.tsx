bf0582537714df9769400b9283c8dd7bf2aac330
Sai Rohit <sairohit11.s@gmail.com>
2023-08-03 21:53:17 +0530

    Header added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from "next/app";
 import Footer from "@/components/footer";
 import "@/styles/styles.css";
+import Header from "@/components/header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header />
       <Component {...pageProps} />
       <Footer />
     </>
