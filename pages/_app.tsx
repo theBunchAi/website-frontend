8e331504656dc53e2a3e304a26cd0da25776800b
Sai Rohit <sairohit11.s@gmail.com>
2023-09-12 22:05:10 +0530

    tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Script from "next/script";
 import Footer from "@/components/footer";
 import "@/styles/styles.css";
@@ -9,6 +10,21 @@ export default function App({ Component, pageProps }: AppProps) {
   // const gaURLTag = "G-7L2RMX12CD";
   return (
     <>
+      <Head>
+        <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MH5RF4FK');`}</script>
+      </Head>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-MH5RF4FK"
+          height="0"
+          width="0"
+          style={{ visibility: "hidden", display: "none" }}
+        ></iframe>
+      </noscript>
       <Script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-7L2RMX12CD"
