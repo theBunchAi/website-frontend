bf4320a3d42d7b5b415fe56d6ef4018a3b516e65
Sai Rohit <sairohit11.s@gmail.com>
2023-09-12 21:42:42 +0530

    tag val change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,7 @@ import HowFooter from "@/components/how-footer";
 import PageBackground from "@/components/page-background";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const gaURLTag = "G-SL6RYF6MQS";
-  const gaURLTag2 = "G-TVR8M7HM7Y";
+  const gaURLTag = "G-7L2RMX12CD";
   return (
     <>
       <Script
@@ -21,21 +20,6 @@ export default function App({ Component, pageProps }: AppProps) {
           function gtag(){window?.dataLayer?.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${gaURLTag}', {
-            page_path : window?.location?.pathname
-          });
-        `}
-      </Script>
-      <Script
-        async
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaURLTag2}`}
-      ></Script>
-      <Script id="g-script2" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window?.dataLayer?.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gaURLTag2}', {
             page_path : window?.location?.pathname
           });
         `}
