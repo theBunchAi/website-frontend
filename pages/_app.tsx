f8d598d6234b78829703f970be89d7706a44e285
Sai Rohit <sairohit11.s@gmail.com>
2023-09-12 20:13:16 +0530

    Added google analytics tag in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Footer from "@/components/footer";
 import "@/styles/styles.css";
 import HowFooter from "@/components/how-footer";
@@ -7,6 +8,19 @@ import PageBackground from "@/components/page-background";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env?.NEXT_PUBLIC_GA_URL_TAG}`}
+      ></Script>
+      <Script id="g-script">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env?.NEXT_PUBLIC_GA_URL_TAG}');
+        `}
+      </Script>
+
       <PageBackground />
       <Component {...pageProps} />
       <Footer />
