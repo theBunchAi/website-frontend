ff46f49834670043b228c2cb81c7f9aefd5fff38
Sai Rohit <sairohit11.s@gmail.com>
2023-09-12 21:53:04 +0530

    tag val new

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,23 +6,21 @@ import HowFooter from "@/components/how-footer";
 import PageBackground from "@/components/page-background";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const gaURLTag = "G-7L2RMX12CD";
+  // const gaURLTag = "G-7L2RMX12CD";
   return (
     <>
       <Script
         async
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaURLTag}`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-7L2RMX12CD"
       ></Script>
-      <Script id="g-script" strategy="afterInteractive">
+      <Script id="ga-script">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window?.dataLayer?.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gaURLTag}', {
-            page_path : window?.location?.pathname
-          });
-        `}
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-7L2RMX12CD');
+  `}
       </Script>
 
       <PageBackground />
